e9aa0535ac9679bc8b3f77093a73d54cdad09c74
Nelson Liu <nelsliu9121@gmail.com>
2017-08-17 14:02:52 +0800

    using cronjobs

--- main.go
+++ main.go
@@ -5,6 +5,5 @@ import (
 )
 
 func main() {
-	tasks.GetLocations()
-	tasks.GetCourses()
+	tasks.Tasks()
 }
--- tasks/tasks.go
+++ tasks/tasks.go
@@ -4,7 +4,20 @@ import (
 	"net/http"
 
 	utils "github.com/nelsliu9121/wgwheretogoserver/utils"
+	"github.com/robfig/cron"
 )
 
 var client = &http.Client{}
 var fb = utils.Firebase()
+var scheduler *cron.Cron
+
+// Tasks Start cron jobs
+func Tasks() {
+	scheduler = cron.New()
+
+	scheduler.AddFunc("0 0 2 1 * *", GetLocations)
+	scheduler.AddFunc("0 0 2 1 * *", GetCourses)
+	scheduler.AddFunc("@daily", GetPosts)
+
+	scheduler.Start()
+}
